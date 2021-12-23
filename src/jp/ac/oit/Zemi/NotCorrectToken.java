2f4f7ff00571e77eda0b45c844f672cc943a2455
tabata <e1b19049@st.oit.ac.jp>
2021-12-24 00:48:57 +0900

    make NotCorrectToken class

---
+++ src/jp/ac/oit/Zemi/NotCorrectToken.java
@@ -0,0 +1,10 @@
+package jp.ac.oit.Zemi;
+
+public class NotCorrectToken extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public NotCorrectToken(String msg) {
+		super(msg);
+	}
+}
